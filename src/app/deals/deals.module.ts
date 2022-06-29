5cf0b7b725a70cbecc4825fabe3611fe80a7d2c9
otavio <persistere.5@gmail.com>
2022-06-29 18:55:11 -0300

    create the interface and list table

--- src/app/deals/deals.module.ts
+++ src/app/deals/deals.module.ts
@@ -1,8 +1,10 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
 
 import { DealsRoutingModule } from './deals-routing.module';
 import { DealsComponent } from './deals/deals.component';
+
 
 
 @NgModule({
@@ -11,7 +13,8 @@ import { DealsComponent } from './deals/deals.component';
   ],
   imports: [
     CommonModule,
-    DealsRoutingModule
+    DealsRoutingModule,
+    MatTableModule
   ]
 })
 export class DealsModule { }
--- src/app/deals/deals/deals.component.ts
+++ src/app/deals/deals/deals.component.ts
@@ -1,15 +1,31 @@
+import { Deals } from './../model/deals';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-deals',
   templateUrl: './deals.component.html',
-  styleUrls: ['./deals.component.scss']
+  styleUrls: ['./deals.component.scss'],
 })
 export class DealsComponent implements OnInit {
+  deals: Deals[] = [
+    {
+      _id: 1,
+      DealName: 'Name of Deeal',
+      PurchasePrice: '$9.000',
+      Address: 'Av Interlagos 4455',
+      NetOperatingIncome: '$23.000',
+      CapRate: '5%',
+    },
+  ];
+  displayedColumns = [
+    'DealName',
+    'PurchasePrice',
+    'Address',
+    'NetOperatingIncome',
+    'CapRate',
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
